0959964de30c1dfaf2dd1ada0cba43b28f5069b0
kindring <43570639+kindring@users.noreply.github.com>
2020-01-02 14:33:53 +0800

    数据库操作

---
+++ database/address.js
@@ -0,0 +1,111 @@
+const pool = require('./MysqlPool');
+function getConnection(){
+    return new Promise((resolve,reject)=>{
+        pool.getConnection((err,conn)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(conn);
+            }
+        })
+    })    
+}
+
+function aquery(conn,sql,value){
+    return new Promise((resolve,reject)=>{
+        conn.query(sql,value,function(err,result){
+            if(err){
+                reject(err);
+            }else{
+                resolve(result);
+            }
+        })
+    })   
+}
+
+async function getProvince(){
+    let [err,conn] = await getConnection().then(conn=>[null,conn]).catch(err=>[err,null]); 
+    if(err){
+        console.log('出现错误');
+        console.log(err);
+        return new Promise((resolve,reject)=>{
+            reject(err);
+        });
+    }
+    let sql = 'select * from bs_province';
+    let value = null;
+    let [error,result] = await aquery(conn,sql,value).then(result=>[null,result]).catch(error=>[error,null]);
+    return new Promise((resolve,reject)=>{
+        if(error){
+            reject(error);
+        }else{
+            resolve(result);
+        }
+    });
+}
+
+
+function getCity(province){
+    return new Promise((resolve,reject)=>{
+        pool.getConnection((err,conn)=>{
+            if(err){
+                return reject(err);
+            }
+            let sql = 'select * from bs_city where PROVINCE_CODE = ?';
+            let value = [province];
+            conn.query(sql,value,function(err,result){
+                if(err){
+                    return reject(err);
+                }
+                resolve(result);
+            })
+
+        })
+
+    })
+}
+
+function getArea(city){
+    return new Promise((resolve,reject)=>{
+        pool.getConnection((err,conn)=>{
+            if(err){
+                return reject(err);
+            }
+            let sql = 'select * from bs_area where city_code = ?';
+            let value = [city];
+            conn.query(sql,value,function(err,result){
+                if(err){
+                    return reject(err);
+                }
+                resolve(result);
+            })
+
+        })
+
+    })
+}
+
+function getStreet(area){
+    return new Promise((resolve,reject)=>{
+        pool.getConnection((err,conn)=>{
+            if(err){
+                return reject(err);
+            }
+            let sql = 'select * from bs_street where area_code = ?';
+            let value = [area];
+            conn.query(sql,value,function(err,result){
+                if(err){
+                    return reject(err);
+                }
+                resolve(result);
+            })
+
+        })
+
+    })
+}
+
+module.exports.getProvince = getProvince;
+module.exports.getCity = getCity;
+module.exports.getArea = getArea;
+module.exports.getStreet = getStreet;
